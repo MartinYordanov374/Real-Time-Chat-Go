6a3343d94ed2ed3005cf5d238a451e96e7efb771
MartinYordanov374 <martinyordanov284@gmail.com>
2026-08-27 23:32:44 +0300

    Add timestamp inside messages

--- frontend/app/ChatBox/ChatBox.tsx
+++ frontend/app/ChatBox/ChatBox.tsx
@@ -4,13 +4,13 @@ import MessageBox from '../MessageBox/MessageBox'
 type message ={
     id: number,
     content: string
-    sender: boolean
+    sender: boolean,
 }
 export default function ChatBox() {
     let Messages : message[] = [{
         id: 1,
         content: "test",
-        sender: true
+        sender: true,
     },
     {
         id: 2,
@@ -36,7 +36,7 @@ export default function ChatBox() {
     <div className="flex flex-col-reverse max-w-md h-screen w-full bg-gray-100 max-h-screen overflow-scroll p-10">
         {Messages.map((message) => (
             <MessageBox 
-            id ={message.id}
+            key ={message.id}
             content={message.content}
             sender={message.sender}/>
         ))}
--- frontend/app/MessageBox/MessageBox.tsx
+++ frontend/app/MessageBox/MessageBox.tsx
@@ -12,14 +12,16 @@ export default function MessageBox(MessageProps : message) {
         {MessageProps.sender 
         ?
         <div className='flex w-full justify-end max-w-[75%]'>
-            <div className='bg-sky-600 rounded-lg p-4'>
+            <div className='bg-sky-600 rounded-lg p-4 mt-2 min-w-[70px]'>
                 {MessageProps.content}
+                <p className='text-xs text-right'>5:40 PM</p>
             </div>
         </div>
         :
         <div className='flex w-full justify-start max-w-[75%]'>
-            <div className='bg-white rounded-lg p-4 text-gray-900'>
+            <div className='bg-white rounded-lg p-4 text-gray-900 mt-2 min-w-[70px]'>
                 {MessageProps.content}
+                <p className='text-xs text-left'>5:40 PM</p>
             </div>
         </div>
         }
