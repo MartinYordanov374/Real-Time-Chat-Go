8a8afc2a46339a942c3840e5763d0bfaa03730c7
MartinYordanov374 <martinyordanov284@gmail.com>
2026-08-28 18:02:03 +0300

    Add basic styling to the chatlist contacts

--- frontend/app/ChatList/ChatList.tsx
+++ frontend/app/ChatList/ChatList.tsx
@@ -1,23 +1,41 @@
 import React from 'react'
 
 export default function ChatList() {
+  const contacts = [{
+    Username: "John Doe",
+    LastMessage: "This is the last message from john doe",
+    LastMessageDate: "Friday"
+  },
+  {
+    Username: "The real John Doe",
+    LastMessage: "This is the last message from THE REAL john doe and he is not playing games!",
+    LastMessageDate: "12:15 PM"
+  }]
   return (
     <div className="justify-left w-[40%] bg-white text-gray-900 border-r">
       {/* 1. User Header */}
-      <div className='border-b '>
+      <div className='border-b pt-4'>
         <h2>Username</h2>
         <p>Active status</p>
-      </div>
+      </div>  
       {/* 2. Search Bar */}
-      <div>
-        <input placeholder='Search users by username'/>
+      <div className='border-b'>
+        <input placeholder='Search users by username' className='p-4 w-full'/>
       </div>
       {/* 3. Contacts List */}
-      <div>
-        <h2>Username</h2>
-        <p>Last message</p>
-        <p>Sent date</p>
-      </div>
+      {contacts.map((Contact) => (
+        <div className='flex flex-col gap-2 p-2 hover:bg-gray-100 cursor-pointer'>
+          <div className='flex'>
+            <h2 className='font-semibold'>{Contact.Username}</h2>
+            <p className='ml-auto text-gray-400 text-sm'>{Contact.LastMessageDate}</p>
+          </div>
+          {/* TODO: Cut out the message after a certain length */}
+          <div className='flex-row'>
+            <p className='text-sm'>{Contact.LastMessage}</p>
+          </div>
+        </div>
+      ))}
+      
     </div>
   )
 }
