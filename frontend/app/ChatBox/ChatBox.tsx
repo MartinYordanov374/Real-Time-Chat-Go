5cdea631163835708da87227df0706e58cf02c5d
MartinYordanov374 <martinyordanov284@gmail.com>
2026-08-28 14:07:25 +0300

    Add receiver header to chat box

--- frontend/app/ChatBox/ChatBox.tsx
+++ frontend/app/ChatBox/ChatBox.tsx
@@ -7,39 +7,71 @@ type message ={
     sender: boolean,
 }
 export default function ChatBox() {
+    let online : boolean = true
     let Messages : message[] = [{
         id: 1,
-        content: "test",
+        content: "test 1",
         sender: true,
     },
     {
         id: 2,
-        content: "Another test",
+        content: "test 2",
         sender: false,
     },
     {
         id: 3,
-        content: "Another test",
+        content: "test 3",
         sender: false,
     },
     {
         id: 4,
-        content: "Another test",
+        content: "test 4",
         sender: true,
     },
     {
         id: 5,
-        content: "Another test",
+        content: "test 5",
         sender: false
+    },
+    {
+        id: 6,
+        content: "test 6",
+        sender: true
+    },
+    {
+        id: 7,
+        content: "test 7",
+        sender: true
+    },
+    {
+        id: 8,
+        content: "test 8",
+        sender: true
+    },
+    {
+        id: 9,
+        content: "test 9",
+        sender: true
     }]
   return (
-    <div className="flex flex-col-reverse max-w-md h-screen w-full bg-gray-100 max-h-screen overflow-scroll p-10">
-        {Messages.map((message) => (
-            <MessageBox 
-            key ={message.id}
-            content={message.content}
-            sender={message.sender}/>
-        ))}
+    <div className="w-full bg-gray-100 max-h-screen overflow-scroll">
+        <div className='border border-gray-900sticky top-0 bg-white'>
+            <h2 className='text-gray-900 pl-8 pt-8'>Username</h2>
+            {online 
+                ?
+                <p className='text-emerald-500 font-bold pl-8 pb-4 border-bottom border-gray-900'> Online </p>
+                :
+                <p className='text-gray-400 pl-8 pb-4 border-bottom border-gray-900'> Offline </p>
+            }
+        </div>
+        <div className='flex flex-col-reverse max-w h-screen p-10'>
+            {Messages.map((message) => (
+                <MessageBox 
+                key ={message.id}
+                content={message.content}
+                sender={message.sender}/>
+            ))}
+        </div>
     </div>
   )
 }
--- frontend/app/MessageBox/MessageBox.tsx
+++ frontend/app/MessageBox/MessageBox.tsx
@@ -11,14 +11,14 @@ export default function MessageBox(MessageProps : message) {
     <div className='MessageWrapper'>
         {MessageProps.sender 
         ?
-        <div className='flex w-full justify-end max-w-[75%]'>
+        <div className='flex w-full justify-end'>
             <div className='bg-blue-500 rounded-lg p-4 mt-2 min-w-[70px]'>
                 {MessageProps.content}
                 <p className='text-xs text-right'>5:40 PM</p>
             </div>
         </div>
         :
-        <div className='flex w-full justify-start max-w-[75%]'>
+        <div className='flex w-full justify-start '>
             <div className='bg-white rounded-lg p-4 text-gray-900 mt-2 min-w-[70px] shadow-lg'>
                 {MessageProps.content}
                 <p className='text-xs text-left'>5:40 PM</p>
