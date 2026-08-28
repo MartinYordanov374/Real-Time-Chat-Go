c590789442d0996508ed85def071a7c8cb442028
MartinYordanov374 <martinyordanov284@gmail.com>
2026-08-28 17:09:55 +0300

    Add chatlist layout

--- frontend/app/ChatList/ChatList.tsx
+++ frontend/app/ChatList/ChatList.tsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function ChatList() {
   return (
-    <div className="justify-left">ChatList</div>
+    <div className="justify-left w-[40%]">
+      {/* 1. User Header */}
+      <div>
+        <h2>Username</h2>
+        <p>Active status</p>
+      </div>
+      {/* 2. Search Bar */}
+      <div>
+        <input placeholder='Search users by username'/>
+      </div>
+      {/* 3. Contacts List */}
+      <div>
+        <h2>Username</h2>
+        <p>Last message</p>
+        <p>Sent date</p>
+      </div>
+    </div>
   )
 }
