5aba1ac871c210d74e4846d747a0dc05aeaea4e0
MartinYordanov374 <martinyordanov284@gmail.com>
2026-09-04 13:59:20 -0400

    User chats are now fetched on the frontend

--- frontend/app/ChatList/ChatList.tsx
+++ frontend/app/ChatList/ChatList.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function ChatList() {
+  // TODO: Make it so that the chat box shows the ID corresponding to the user on the other end of the chat, 
+  // i.e., if User A is retrieving the chats, they shall see user B in the contacts list
   const contacts = [{
     Username: "John Doe",
     LastMessage: "This is the last message from john doe",
@@ -13,6 +15,7 @@ export default function ChatList() {
   }]
   return (
     <div className="justify-left w-[50%] bg-white text-gray-900">
+      {/* TODO: Populate the user header with data about the current user */}
       {/* 1. User Header */}
       <div className='flex shadow-xs p-4'>
         <div className='flex flex-col size-12 rounded-full bg-blue-500 p-2 justify-center items-center'>PFP</div>
@@ -21,12 +24,14 @@ export default function ChatList() {
           <span className='text-sm text-gray-400 pt-0'>Online</span>
         </div>
       </div>  
+      {/* TODO: Implement search existing AND new contacts */}
       {/* 2. Search Bar */}
       <div className='p-4'>
         <input 
         className="p-4 w-full focus:outline-none bg-gray-200 rounded-xl text-sm"
         placeholder='Find contacts by username'/>
       </div>
+      {/* TODO: List all of the chats that the user is a member of. Populate the data accordingly. */}
       {/* 3. Contacts List */}
       {contacts.map((Contact) => (
         <div className='flex flex-col gap-2 p-4 hover:bg-gray-100 cursor-pointer'>
--- frontend/app/RegisteredHome/page.tsx
+++ frontend/app/RegisteredHome/page.tsx
@@ -1,8 +1,28 @@
+"use client"
 import ChatBox from "../ChatBox/ChatBox";
 import ChatList from "../ChatList/ChatList";
+import Axios from 'axios'
+import {useState, useEffect} from 'react'
 export default function page() {
+  // TODO: First fetch the chats and then load the page to ensure no issues with the page loading before the chats are retrieved occur
+  const [chats, setChats] = useState()
+
+  useEffect(() => {
+    async function RetrieveUserChats(){
+      let AllCurrentUserChats = await Axios.get('http://localhost:8080/GetAllChats', {withCredentials: true})
+      .then((res) =>{
+        setChats(res.data?.chats)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+    RetrieveUserChats()
+  }, [])
+
   return (
     <div className="flex">
+        {/* TODO: Send the chats list to the chat list component and populate the contacts page with the chats */}
         <ChatList/>
         {/* TODO: Selecting a contact from the chat list 
         will open the chat box with the corresponding conversation */}
