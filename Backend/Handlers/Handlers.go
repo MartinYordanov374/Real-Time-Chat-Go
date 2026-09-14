163d27362e34ec5b7a4ecc6fc4d152bae24f0cdf
MartinYordanov374 <martinyordanov284@gmail.com>
2026-09-14 23:30:49 +0300

    Add mongo operators based matching for searching a user by username

--- Backend/Handlers/Handlers.go
+++ Backend/Handlers/Handlers.go
@@ -241,7 +241,12 @@ func GetCurrentUserData(GinContext *gin.Context) {
 func SearchUser(GinContext *gin.Context) {
 	TargetUsername := GinContext.Param("Username")
 	var TargetUser MongoConfig.User
-	filter := bson.M{"username": TargetUsername}
+	filter := bson.M{
+		"username": bson.M{
+			"$gte": TargetUsername + "\uffff",
+			"$lt":  TargetUsername + "\uffff",
+		},
+	}
 	// NOTE: This functionality should first look for the user's CONTACTS(chats with users) and then look for other users.
 	// If a user is found and they do not have a chat with them, the user's username and ID should be displayed alone in the
 	// contacts list (a username should be unique). The user is given the option to send a message to the said person.
