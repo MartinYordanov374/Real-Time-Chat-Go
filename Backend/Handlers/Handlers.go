2c4560c95be1dd0ecf20f47e7a6b17e59d59d1a9
MartinYordanov374 <martinyordanov284@gmail.com>
2026-09-14 23:57:22 +0300

    Move find users function to a separate file in helper functions

--- Backend/Handlers/Handlers.go
+++ Backend/Handlers/Handlers.go
@@ -240,20 +240,14 @@ func GetCurrentUserData(GinContext *gin.Context) {
 
 func SearchUser(GinContext *gin.Context) {
 	TargetUsername := GinContext.Param("Username")
-	var TargetUser MongoConfig.User
-	filter := bson.M{
-		"username": bson.M{
-			"$gte": TargetUsername,
-			"$lt":  TargetUsername + "\uffff",
-		},
-	}
+
 	// NOTE: This functionality should first look for the user's CONTACTS(chats with users) and then look for other users.
 	// If a user is found and they do not have a chat with them, the user's username and ID should be displayed alone in the
 	// contacts list (a username should be unique). The user is given the option to send a message to the said person.
 	// After the message has been sent a box appears that asks the user to wait for the other user to accept the chat request.
 	// TODO: Rewrite this to match usernames by regex or soemthing once you get the functionality working for a specific Username
 	// TODO: Do not return the entire user object, just the information necessary
-	err := GlobalVariables.MongoUsersCollection.FindOne(context.Background(), filter).Decode(&TargetUser)
+	TargetUser, err := HelperFunctions.FindUsers(TargetUsername)
 
 	if err != nil {
 		log.Println(err)
